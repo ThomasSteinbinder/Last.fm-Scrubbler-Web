1ba831a8eeac83191509fc6350f9306d62080a2a
Thomas Steinbinder <thomas.steinbinder@onlinehome.de>
2022-12-23 19:31:26 +0100

    utils module

---
+++ utils.js
@@ -0,0 +1,12 @@
+function removeEmptyPropsFrom(obj) {
+  for (var propName in obj) {
+    if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "") {
+      delete obj[propName];
+    }
+  }
+  return obj
+}
+
+module.exports = {
+  removeEmptyPropsFrom,
+}
