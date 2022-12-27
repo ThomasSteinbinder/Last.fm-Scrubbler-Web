a3529c9fd2622ea3b8143985ac9fb8a57fac16cd
Thomas Steinbinder <thomas.steinbinder@onlinehome.de>
2022-12-27 12:55:11 +0100

    isAuthenticated function

--- authentication.js
+++ authentication.js
@@ -1,37 +1,46 @@
 const LastFm = require("lastfm-node-client");
 
 async function getLastFmUser(session_key) {
-  try {
-    const lastFm = new LastFm(API_KEY, SECRET, session_key);
-    const user = await lastFm.userGetInfo();
-    return user.user;
-  } catch (err) {
-    return null;
-  }
+    try {
+        const lastFm = new LastFm(API_KEY, SECRET, session_key);
+        const user = await lastFm.userGetInfo();
+        return user.user;
+    } catch (err) {
+        return null;
+    }
 }
 
 function getLastFmObjectFrom(req) {
-  const sessionKey = req.cookies.session_key;
-  const lastFm = new LastFm(API_KEY, SECRET, sessionKey)
-  return lastFm;
+    const sessionKey = req.cookies.session_key;
+    const lastFm = new LastFm(API_KEY, SECRET, sessionKey)
+    return lastFm;
+}
+
+async function isAuthenticated(req) {
+    const session_key = req.cookies.session_key;
+    const username = req.cookies.username;
+    const user = await getLastFmUser(session_key);
+    return (user && username == user.name)
 }
 
 async function validateSession(req, res, next) {
-  const session_key = req.cookies.session_key;
-  const username = req.cookies.username;
-  const user = await getLastFmUser(session_key);
-  if (user && username == user.name) {
-    res.cookie("user", JSON.stringify(user));
-    next();
-  } else {
-    res.clearCookie("session_key");
-    res.clearCookie("username");
-    res.redirect("/")
-  }
+    const session_key = req.cookies.session_key;
+    const username = req.cookies.username;
+    const user = await getLastFmUser(session_key);
+    
+    if (user && username == user.name) {
+        res.cookie("user", JSON.stringify(user));
+        next();
+    } else {
+        res.clearCookie("session_key");
+        res.clearCookie("username");
+        res.redirect("/")
+    }
 }
 
 module.exports = {
-  getLastFmUser,
-  getLastFmObjectFrom,
-  validateSession,
+    getLastFmUser,
+    getLastFmObjectFrom,
+    isAuthenticated,
+    validateSession,
 }
