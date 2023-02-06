a9d98e984291ab8d9100a536dd6b48ed317db90e
Thomas Steinbinder <thomas.steinbinder@onlinehome.de>
2023-02-06 17:40:50 +0100

    getLastFmUser from user name

--- authentication.js
+++ authentication.js
@@ -1,46 +1,52 @@
 const LastFm = require("lastfm-node-client");
 
-async function getLastFmUser(session_key) {
-    try {
-        const lastFm = new LastFm(API_KEY, SECRET, session_key);
-        const user = await lastFm.userGetInfo();
-        return user.user;
-    } catch (err) {
-        return null;
+async function getLastFmUser(session_key, username = undefined) {
+  try {
+    const lastFm = new LastFm(API_KEY, SECRET, session_key);
+    let user = undefined;
+    if (username == undefined) {
+      user = await lastFm.userGetInfo();
+    } else {
+      user = await lastFm.userGetInfo({ user: username });
     }
+    return user.user;
+  } catch (err) {
+    console.log("oh boii...")
+    return null;
+  }
 }
 
 function getLastFmObjectFrom(req) {
-    const sessionKey = req.cookies.session_key;
-    const lastFm = new LastFm(API_KEY, SECRET, sessionKey)
-    return lastFm;
+  const sessionKey = req.cookies.session_key;
+  const lastFm = new LastFm(API_KEY, SECRET, sessionKey)
+  return lastFm;
 }
 
 async function isAuthenticated(req) {
-    const session_key = req.cookies.session_key;
-    const username = req.cookies.username;
-    const user = await getLastFmUser(session_key);
-    return (user && username == user.name)
+  const session_key = req.cookies.session_key;
+  const username = req.cookies.username;
+  const user = await getLastFmUser(session_key);
+  return (user && username == user.name)
 }
 
 async function validateSession(req, res, next) {
-    const session_key = req.cookies.session_key;
-    const username = req.cookies.username;
-    const user = await getLastFmUser(session_key);
-    
-    if (user && username == user.name) {
-        res.cookie("user", JSON.stringify(user));
-        next();
-    } else {
-        res.clearCookie("session_key");
-        res.clearCookie("username");
-        res.redirect("/")
-    }
+  const session_key = req.cookies.session_key;
+  const username = req.cookies.username;
+  const user = await getLastFmUser(session_key);
+
+  if (user && username == user.name) {
+    res.cookie("user", JSON.stringify(user));
+    next();
+  } else {
+    res.clearCookie("session_key");
+    res.clearCookie("username");
+    res.redirect("/")
+  }
 }
 
 module.exports = {
-    getLastFmUser,
-    getLastFmObjectFrom,
-    isAuthenticated,
-    validateSession,
+  getLastFmUser,
+  getLastFmObjectFrom,
+  isAuthenticated,
+  validateSession,
 }
